1f6b0dd890be2a46f946ec2bf17185ce8b549e1e
Meekrat34 <dhebard@gmail.com>
2017-03-24 16:19:42 -0500

    initial draft
    First draft. may fail as I have not double checked some of the names yet.
    I listed at the bottom the ones that I suspect could be wrong.

--- install_packages.sh
+++ install_packages.sh
@@ -2,14 +2,16 @@
 #
 # WOOT WOOT WOOT
 #
+clear   ## I love to start with a clean screen, don't you?
+#
 # If you are here then you have a bootable system....
 #                     ..... with network
 #                           ..... no error
 #                                 ..... a nice lean,clean linux machine.
 # Soooo, let's fatten it up a bit shall we
 #
-# Common packages
-COMM='mc tree git dialog openssh memtest86+ bsd-games samba cdrtools cifs-utils udiskie unrar unzip p7zip'
+# Common packages, no matter what machine setup I build.
+COMM='mc sudo tree git dialog tmux openssh memtest86+ bsd-games samba cdrtools cifs-utils udiskie unrar unzip p7zip'
 #
 # Make a choice of packages to install.
 #
@@ -17,7 +19,22 @@ COMM='mc tree git dialog openssh memtest86+ bsd-games samba cdrtools cifs-utils 
 # PAC_B - Simple server setup, no gui
 # PAC_C - Studio type with Video, Audio, Image, and 3D model editors and DVD mastering tools
 #
-#
-PAC_A='lxde xorg vlc'
-PAC_B='tmux lynx binutils ddrescue e2fsprogs hdparm'
+PAC_A='lxde lxdm xorg vlc Qt4'
+PAC_B='lynx binutils ddrescue e2fsprogs hdparm'
 PAC_C='vlc gimp 3Dblender'
+#
+# Extra packages that I have found usefull, but not all the time
+XTRA='parted gparted ddrescue ddrescue-gui'#
+# If you don't want extra then remove variable from below
+# Also change the package letter to match your ultimate desire for you machine
+#
+
+pacman -S $COMM $PAC_A $XTRA
+
+# vlc for command line?
+#
+# Verify the names of the following. caps, spelling, special symbols, etc. They MUST be excact or they will fail.
+# 3dblender ddrescue-gui Qt4
+
+
+
