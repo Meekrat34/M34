a035cce5f1f79aa98cd12eec52a86cc59096db24
Meekrat34 <dhebard@gmail.com>
2017-03-25 12:08:18 -0500

    cleanup and package edit
    +added utility and codec section to keep things a bit tidy
    -moved memtest86 to an earlier script

--- install_packages.sh
+++ install_packages.sh
@@ -6,12 +6,21 @@ clear   ## I love to start with a clean screen, don't you?
 #
 # If you are here then you have a bootable system....
 #                     ..... with network
-#                           ..... no error
+#                           ..... no errors
 #                                 ..... a nice lean,clean linux machine.
 # Soooo, let's fatten it up a bit shall we
 #
-# Common packages, no matter what machine setup I build.
-COMM='mc sudo tree git tig dialog tmux openssh memtest86+ bsd-games samba cdrtools cifs-utils udiskie unrar unzip p7zip veracrypt'
+## Common packages, no matter what machine setup I build.
+COMM='mc sudo tree git tig dialog tmux openssh bsd-games samba cdrtools cifs-utils udiskie unrar unzip p7zip p7zip-plugins rsync veracrypt'
+#
+## Codecs' for video, audio, and reading DVDs'
+CODEC='a52dec faac faad2 flac jasper lame libdca libdv gst-libav libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore gstreamer0.10-plugins libdvdcss libdvdread libdvdnav dvd+rw-tools dvdauthor dvgrab'
+#
+## Some extra programs ( not always needed )
+XTRA='parted gparted ddrescue hdparm'
+#
+## Some utility programs ( not always needed )
+UTILS='exfat-utils fuse-exfat '
 #
 # Make a choice of packages to install.
 #
@@ -20,16 +29,15 @@ COMM='mc sudo tree git tig dialog tmux openssh memtest86+ bsd-games samba cdrtoo
 # PAC_C - Studio type with Video, Audio, Image, and 3D model editors and DVD mastering tools
 #
 PAC_A='lxde lxdm xorg vlc qt4'
-PAC_B='lynx binutils ddrescue e2fsprogs hdparm'
-PAC_C='vlc gimp blender audacity'
+PAC_B='lynx binutils e2fsprogs'
+PAC_C='vlc gimp blender audacity clementine'
 #
 # Extra packages that I have found usefull, but not all the time
-XTRA='parted gparted ddrescue'
 # If you don't want extra then remove variable from below
 # Also change the package letter to match your ultimate desire for you machine
 #
 
-pacman -S $COMM $PAC_A $XTRA
+pacman -S $COMM $PAC_A $XTRA $CODEC $UTILS
 
 # vlc for command line?
 #
