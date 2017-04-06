e37e9abeb0fd7341698192eaf08c737570941eaf
Meekrat34 <dhebard@gmail.com>
2017-04-06 16:24:06 -0500

    added color to the showme script file, plus cleaned up the code some more.

--- scripts/showme.sh
+++ scripts/showme.sh
@@ -1,31 +1,53 @@
 #!/bin/bash
 clear
-if [ -f "settings.conf" ]; then
-      . "./settings.conf"
+if [ -f "./settings.conf" ]; then
+      source "./settings.conf"; else
+	source "../settings.conf"
 fi
+#
+# Set up some colors
+RED='\033[0;31m' # Red txt
+GRN='\033[0;32m' # Green txt
+YLW='\033[0;33m' # Yellow txt
+BLUE='\033[0;34m' # Blue txt
+DECO='\033[0m' # Default color
+
 #
 # This is only a script to show variables from settings.conf
 #
 # and to test some of its functions
 #
 echo
+printf " Current connected drives are listed here..."
+echo
+lsblk
+echo
+#
+#
+printf " These are the variables in the settings.conf file. Adjust as needed."
+echo
 printf " Root = $DEST_ROOT\n"
 printf " Home = $DEST_HOME\n"
 printf " Swap = $DEST_SWAP\n"
 echo
-printf "Base package installs are -> $PACFILES\n"
+printf "Base package installs are -> (${GRN}$PACFILES${DECO})\n"
 echo
+printf " Localization is (${RED}$LOC_GEN${DECO}) and (${GRN}$LOC_CONF${DECO})\n"
 echo
-printf " Localization is $LOC_GEN\n"
-printf "            and $LOC_CONF\n"
+printf " Hostname is (${RED}$HOSTNAME${DECO}), Groups for (${GRN}$USERNAME${DECO}) are (${RED}$USERGRP${DECO}) 
+	 Your users chosen shell is (${YLW}$SHELL${DECO})\n"
 echo
+printf " Current Bootloader files to download are -> (${BLUE}$BOOTLDR${DECO})\n"
 echo
-printf " Hostname is $HOSTNAME\n"
-printf " Groups for $USERNAME are $USERGRP\n"
-printf " Chosen shell is $SHELL\n"
-
+printf " Common packages are ${GRN}$COMM${DECO}\n"
+echo
+printf " Package A is ${GRN}$PAC_A${DECO}\n"
+echo 
+printf " Package B is ${GRN}$PAC_B${DECO}\n"
+echo 
+printf " Package C is ${GRN}$PAC_C${DECO}\n"
+echo
 
 
 
 PAUSEME
-done
