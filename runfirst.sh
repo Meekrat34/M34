6f30160f128ceeb431f381e88451f74c78b53241
Meekrat34 <dhebard@gmail.com>
2017-04-06 15:28:34 -0500

    First draft
    save some time with this file

---
+++ runfirst.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Set the executable flag on the following script files
+#
+chmod a+x ./menu.sh
+chmod a+x ./scripts/install_base.sh
+chmod a+x ./scripts/install_newroot.sh
+chmod a+x ./scripts/install_packages.sh
+chmod a+x ./scripts/install_syslinux.sh
+chmod a+x ./scripts/showme.sh
