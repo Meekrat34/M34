657b493f8ff3e20dcfc1efbdfa6b929140ebf4d5
Meekrat34 <dhebard@gmail.com>
2017-05-12 14:13:03 -0500

    Net check and update
    +added ping
    +added update pacman database
    -removed the combine variable and reverted to individual variables

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -15,13 +15,24 @@ if [ -f "./settings.conf" ]; then
       printf "Settings file not found"; exit
 fi
 #
+echo
+ping -c 3 www.google.com
+echo
+printf "Make sure you can ping the internet"
+echo
+PAUSEME
+echo
+UPDPAC            # update pacman database
+
 # And now, let's fatten it up a bit shall we
-pacman -S $COMBINE_THEM
+#pacman -S $COMBINE_THEM
+pacman -S $COMM $CODEC $XTRA $UTILS $PAC_A
+
 
 # vlc for command line? research this
 #
 # Verify the names of the following. caps, spelling, special symbols, etc. They MUST be excact or they will fail.
 # ddrescue-gui = defunct
-
+PAUSEME
 
 
